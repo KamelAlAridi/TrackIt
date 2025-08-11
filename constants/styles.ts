3a7a69d667a7e7a1d58bdbfcc4ee87ad091c77d2
Kamel Al Aridi <kamelalaridi@gmail.com>
2025-08-11 22:45:51 +0300

    added navigation logic and basic app ui setup

---
+++ constants/styles.ts
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: "#f0f9ff",
+    primary100: "#e0f2fe",
+    primary200: "#bae6fd",
+    primary400: "#0ea5e9",
+    primary500: "#0284c7",
+    primary700: "#0369a1",
+    primary800: "#075985",
+    accent500: "#10b981",
+    error50: "#fef2f2",
+    error500: "#ef4444",
+    gray500: "#64748b",
+    gray700: "#334155",
+  },
+};
