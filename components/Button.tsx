d631483f1f57bd13e91942afb913bf1022457177
Kamel Al Aridi <kamelalaridi@gmail.com>
2025-08-20 16:27:04 +0300

    fixed custom button component background color typo

--- components/Button.tsx
+++ components/Button.tsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     backgroundColor: GlobalStyles.colors.primary500,
   },
   flat: {
-    backgroundColor: "trasparent",
+    backgroundColor: "transparent",
   },
   buttonText: {
     color: "white",
