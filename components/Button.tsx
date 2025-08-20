867c27d6f831e1978b1e33947cbc40de7b1b223e
Kamel Al Aridi <kamelalaridi@gmail.com>
2025-08-20 16:21:25 +0300

    added a custom button component

---
+++ components/Button.tsx
@@ -0,0 +1,46 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import React, { Children, ReactNode } from "react";
+import { GlobalStyles } from "../constants/styles";
+
+type Props = {
+  children: ReactNode;
+  onPress: () => void;
+  mode: string;
+};
+
+export default function Button({ children, onPress, mode }: Props) {
+  return (
+    <View>
+      <Pressable onPress={onPress}>
+        <View style={[styles.button, mode === "flat" && styles.flat]}>
+          <Text style={[styles.buttonText, mode === "flat" && styles.flatText]}>
+            {children}
+          </Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 4,
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary500,
+  },
+  flat: {
+    backgroundColor: "trasparent",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  flatText: {
+    color: GlobalStyles.colors.primary200,
+  },
+  pressed: {
+    opacity: 0.75,
+    backgroundColor: GlobalStyles.colors.primary100,
+    borderRadius: 4,
+  },
+});
